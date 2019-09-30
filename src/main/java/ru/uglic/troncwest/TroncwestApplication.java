421c9d080e0c83422b5a702468aafd145e6f31df
Stepan Shcherbakov <me@uglic.ru>
2019-09-30 09:45:30 +0300

    Initial commit

---
+++ src/main/java/ru/uglic/troncwest/TroncwestApplication.java
@@ -0,0 +1,13 @@
+package ru.uglic.troncwest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TroncwestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TroncwestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/uglic/troncwest/TroncwestApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.uglic.troncwest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TroncwestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
